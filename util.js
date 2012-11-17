fde39aa8a0c00cefcaa258773abe7676f0631484
Aaron Bell <aaron.bell@gmail.com>
2012-11-17 16:34:32 -0500

    added Pointer, not quite there yet

--- util.js
+++ util.js
@@ -70,7 +70,8 @@ function init3d(far) {
   MATS.yellow = new THREE.MeshLambertMaterial({ color : 0xFFFF00 });
   MATS.normal = new THREE.MeshNormalMaterial();
 
-  MATS.lineVertex = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors } );
+  // TODO linewidth is broken https://github.com/mrdoob/three.js/issues/269
+  MATS.lineVertex = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors, linewidth: 1 } );
 
   STATS = new Stats();
   STATS.domElement.style.position = 'absolute';
@@ -98,6 +99,36 @@ MY3.Line.prototype = Object.create(THREE.Line.prototype);
 MY3.Line.prototype.setEnd = function(position) {
   // no op - will this animate/update correctly if we change the geometry?
 };
+
+// 1. Point along a normalized vector, or
+// TODO 2. Point at another arbitrary position if the pointAt arg is present
+// default length is 200
+MY3.Pointer = function(position, direction, length, pointAt) {
+  var length = (typeof length === "undefined") ? 200 : length;
+  if (typeof pointAt === "undefined") {
+    // use a normal vector
+    if (!MY3.isNormal(direction)) throw ('direction must be a normal, length: ' + direction.length());
+    var endPoint = direction.clone().multiplyScalar(length);
+  endPoint.add(endPoint, position);
+  } else {
+    // point at something
+
+  }
+
+  var lineGeometry = new THREE.Geometry();
+  lineGeometry.vertices.push(position);
+  lineGeometry.vertices.push(endPoint);
+  lineGeometry.colors.push(new THREE.Color( 0x00aa00 ));
+  lineGeometry.colors.push(new THREE.Color( 0xffffff ));
+  THREE.Line.call(this, lineGeometry, MATS.lineVertex); // super constructor
+}
+MY3.Pointer.prototype = Object.create(THREE.Line.prototype);
+
+// true if the vector length is within a small delta of 1
+MY3.isNormal = function(vector) {
+  var diff = Math.abs(1 - vector.length());
+  return (diff < 0.01);
+}
 
 // mat is optional, defaults to yellow
 // req: scene
